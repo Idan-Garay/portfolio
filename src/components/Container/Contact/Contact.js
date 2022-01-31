d906c2037d9451b358e3e2108839d6de6169957f
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-01-31 16:12:39 +0800

    Designed Contact Section

--- src/components/Container/Contact/Contact.js
+++ src/components/Container/Contact/Contact.js
@@ -3,17 +3,25 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>
-      <h1 className="section-title">Drop a message</h1>
-      <form>
+    <div className="contact">
+      <h2 className="section-title">Drop a message</h2>
+      <form className="email-form">
         <div className="input-group">
           <label htmlFor="email">Email</label>
           <input name="email" id="email" />
         </div>
 
         <div className="input-group">
-          <label htmlFor="email">Email</label>
-          <input name="email" id="email" />
+          <label htmlFor="subject">Subject</label>
+          <input name="subject" id="subject" />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" />
+        </div>
+        <div className="input-group submit-cont">
+          <button type="submit">send message</button>
         </div>
       </form>
     </div>
