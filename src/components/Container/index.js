a74997894ce37606ef014c018b9992b63c52cb01
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-10-02 12:35:51 +0800

    Initial commit (via bun create)

--- src/components/Container/index.js
+++
@@ -1,7 +0,0 @@
-import Navbar from "./Navbar/Navbar";
-import Header from "./Header/Header";
-import About from "./About/About";
-import Projects from "./Projects/Projects";
-import Contact from "./Contact/Contact";
-
-export { Navbar, Header, About, Projects, Contact };
