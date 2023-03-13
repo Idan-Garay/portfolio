268a62c4a05c9d321620f47a13db1ee31e428198
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2023-03-13 18:35:32 +0800

    fix: make navigation responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {BsLinkedin, BsGithub, BsSun}  from 'react-icons/bs'
 
 export default function Home() {
   return (
@@ -12,12 +13,22 @@ export default function Home() {
         <link rel="icon" href="/icon.png" />
       </Head>
 
-      <main className="min-h-[100vh] h-full w-full">
-        <nav className="mx-auto mt-[15px] font-medium flex justify-center gap-x-14 items-center min-w-[400px] min-h-[60px] h-[60px] border w-[400px] rounded-full text-base uppercase">
+      <main className="min-h-[100vh] h-full w-full ">
+        <header className='w-full border flex justify-center pt-[15px] px-[15%]'>
+          <nav className="mx-auto flex justify-center gap-x-8 md:gap-x-14 items-center min-w-[280px] md:min-w-[400px] min-h-[50px] h-[50px] border w-[400px] rounded-full text-sm font-semibold uppercase text-gray-700">
             <Link className='' href={"/"}>Projects </Link>
             <Link className='' href={"/"}>About</Link>
             <Link className='' href={"/"}>Contact</Link>
-        </nav>
+          </nav>
+          <div className="nav-tools flex items-center border md:gap-x-3 ">
+            <Link className='hidden md:block' href={'/'}><BsGithub size={25}/></Link>
+            <Link className='hidden md:block' href={'/'}><BsLinkedin size={25}/></Link>
+            <div className="rounded-full border h-[30px] w-[30px]">
+              <BsSun size={25} className='m-auto'/>
+            </div>
+          </div>
+        </header>
+
       </main>
 
       <footer className="">
