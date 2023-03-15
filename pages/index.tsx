a32dd7ba63b5e2a59e761da3af8cfacc6a4f88e9
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2023-03-16 01:10:59 +0800

    fix: change height on small screen size

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,7 +50,7 @@ export default function Home() {
         </div>
 
         <div className="w-full h-[100vh] bg-secondary flex px-[10vw] flex-col  gap-y-6 items-center justify-center ">
-          <div className="group content w-full flex flex-col-reverse md:flex-row justify-center gap-x-24 h-[70%] sm:h-[80%] md:h-[45%] lg:h-[60%] -mt-[5vh]">
+          <div className="group content w-full flex flex-col-reverse md:flex-row justify-center gap-x-24 h-[75%] sm:h-[80%] md:h-[45%] lg:h-[60%] -mt-[5vh]">
             <div className="showcase w-full self-start">
               <Link className='flex flex-col items-end gap-y-3' href="https://nextjs-typescript.d3k5bbpd7nkhfg.amplifyapp.com/" target="_blank">
                 <Image src="/handyhome.png" className=' duration-500 group-hover:outline group-hover:outline-offset-2 group-hover:outline-highlight group-hover:rounded-md' alt="handyhome_preview" width={780} height={1024} />
