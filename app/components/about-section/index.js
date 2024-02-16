ea97ed418c672213f8dab76a046489957f0c9318
Idan <garayidan@gmail.com>
2024-02-16 11:05:07 +0800

    fix: add overflow-hidden on .left

--- app/components/about-section/index.js
+++ app/components/about-section/index.js
@@ -2,18 +2,33 @@ import { roboto } from "@/app/fonts";
 import Image from "next/image";
 import styles from "./index.module.css";
 import Link from "next/link";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 
 export const AboutSection = () => {
+  const ref = useRef();
+  const isInView = useInView(ref, { once: false });
+
+  const leftToRight = {
+    transform: isInView ? "none" : "translateX(-150px)",
+    opacity: isInView ? 1 : 0,
+    transition: "all 0.8s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+  };
+
+  const rightToLeft = {
+    transform: isInView ? "none" : "translateX(150px)",
+    opacity: isInView ? 1 : 0,
+    transition: "all 0.8s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+  };
+
   return (
     <section id="about" className={styles.aboutSection}>
       <nav
         style={{
-          // padding: "0 5%",
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
           height: "10%",
-          paddingRight: "7%",
         }}
       >
         <div
@@ -40,21 +55,31 @@ export const AboutSection = () => {
 
       <div className={styles.sectionContent}>
         <div className={styles.left}>
-          <h1 style={roboto.style} className={styles.csGraduate}>
+          <h1
+            ref={ref}
+            style={{ ...roboto.style, ...leftToRight }}
+            className={styles.csGraduate}
+          >
             CS Graduate
           </h1>
           <div className={styles.usc}>
             <div className={styles.uscLine}> </div>
-            <h5 className={styles.uscText}>University of San Carlos</h5>
+            <h5 className={styles.uscText} style={rightToLeft}>
+              University of San Carlos
+            </h5>
           </div>
-          <p className={styles.p}>
+          <p className={styles.p} style={rightToLeft}>
             Almost 2 years of work experience as a front-end, and full-stack
             developer
           </p>
 
-          <div className={styles.leftPersonaImgLabel}>
+          <div className={styles.leftPersonaImgLabel} >
             <p
-              style={{ paddingTop: "0", fontSize: "var(--font-size-base)" }}
+              style={{
+                paddingTop: "0",
+                fontSize: "var(--font-size-base)",
+                ...leftToRight,
+              }}
               className={styles.p}
             >
               I find it time-saving to meticulously plan the <br />
@@ -64,8 +89,9 @@ export const AboutSection = () => {
           </div>
         </div>
         <div className={styles.right}>
-          <div className={styles.sanctions}>
+          <div style={rightToLeft} className={styles.sanctions}>
             <Image
+              style={rightToLeft}
               className={styles.greenWall}
               src="/green-wall.webp"
               alt="green-wall"
@@ -84,7 +110,11 @@ export const AboutSection = () => {
                 <div className={styles.personaImgLabelLine}></div>
                 <p
                   className={styles.p}
-                  style={{ fontSize: "var(--font-size-base)" }}
+                  style={{
+                    fontSize: "var(--font-size-base)",
+                    ...rightToLeft,
+                    transitionDelay: "0.9s",
+                  }}
                 >
                   I find it time-saving to meticulously plan the <br />
                   implementation before diving into coding
