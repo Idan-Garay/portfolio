ffc608b745065a113af67284c28e4ecd2dff134e
Idan <garayidan@gmail.com>
2024-02-16 10:14:29 +0800

    fix: connect hero and about sections

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 "use client";
 import { HeroSection } from "./components/hero-section";
 import { AboutSection } from "./components/about-section";
-import { useScroll, useTransform, motion } from "framer-motion";
-import { useRef } from "react";
 
 export default function Home() {
   return (
     <>
-      {/* <HeroSection /> */}
+      <HeroSection />
       <AboutSection />
     </>
   );
