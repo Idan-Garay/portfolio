f17f512af7efe4eebd9da07fe7012bff31bf3697
Idan <garayidan@gmail.com>
2024-02-16 16:17:28 +0800

    fix: add transitions to exp-skills-section

--- app/components/exp-skills-section/index.js
+++ app/components/exp-skills-section/index.js
@@ -1,8 +1,10 @@
 import styles from "./index.module.css";
 import { SectionNavigation } from "../section-navigation";
 import Link from "next/link";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
 
-const Skill = ({ name = "JavaScript" }) => {
+const Skill = ({ name = "JavaScript", index, style }) => {
   return (
     <div
       style={{
@@ -12,6 +14,8 @@ const Skill = ({ name = "JavaScript" }) => {
         gap: "1rem",
         alignItems: "center",
         minWidth: "9rem",
+        ...style,
+        transitionDelay: `${50 * index}ms`,
       }}
     >
       <div
@@ -39,17 +43,25 @@ const skills = [
 ];
 
 export const ExpSkillsSection = () => {
+  const ref = useRef();
+  const isInView = useInView(ref, { once: false });
+  const skillStyle = {
+    transition: "all 0.8s",
+    opacity: isInView ? 1 : 0,
+    transform: isInView ? "none" : "translateX(-150px)",
+  };
+
   return (
     <section id="expskillssection" className={styles.expSkillsSection}>
       <SectionNavigation title="Skills & Experience" />
       <div className={styles.sectionContent}>
-        <p className={styles.p}>
+        <p className={styles.p} style={skillStyle}>
           My area of concentration revolves around JavaScript. At present, I use
           NextJS Framework as a Full-stack framework to integrate 3rd Party
           Services and Shadcn-ui to quickly build front-end components.
         </p>
 
-        <span>
+        <span style={skillStyle}>
           Visit my{" "}
           <Link
             href="https://www.linkedin.com/in/idan-joshua-garay"
@@ -60,9 +72,9 @@ export const ExpSkillsSection = () => {
           for more
         </span>
 
-        <div className={styles.skills}>
+        <div className={styles.skills} ref={ref}>
           {skills.map((val, _) => (
-            <Skill key={_} name={val} />
+            <Skill key={_} index={_} name={val} style={skillStyle} />
           ))}
         </div>
       </div>
