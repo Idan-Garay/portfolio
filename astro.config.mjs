b873b75558fa592ee3232564c4cfbf47c7be1deb
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2026-04-13 16:51:03 +0800

    Add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [react()],
 
   vite: {
